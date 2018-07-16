afe1d7d1ccfcea44f509f1bb1e51059fcfb8257d
Ahmed Ali <kingcocomango@gmail.com>
2018-07-16 22:09:30 +0200

    Added orderbook and positions

--- test/browser-exchange/jsx/app.jsx
+++ test/browser-exchange/jsx/app.jsx
@@ -1,5 +1,7 @@
 import { h, render, Component } from 'preact'
 
+import { exampleBook, examplePosition } from './data'
+
 const readNodeConf = {
   httpEndpoint: 'http://localhost:8888',
   keyProvider: [
@@ -76,12 +78,121 @@ class Orderbook extends Component {
     clearInterval(this.periodicFetch)
   }
 
-  render (props, state) {
-    if (state.error) {
-      return <ErrorBox error={state.error.message} />
+  render () {
+    const {
+      cancelcb,
+      symbol,
+      decimals,
+    } = this.props
+
+    const {
+      bids,
+      asks,
+      error
+    } = this.state
+
+    return (
+      <OrderbookInternal
+        bids={bids}
+        asks={asks}
+        error={error}
+        decimals={decimals}
+        cancelcb={(id, side) => {cancelcb(id, symbol, side)}}
+      />
+    )
+  }
+}
+
+class OrderbookInternal extends Component {
+  render() {
+    const {
+      bids = [],
+      asks =  [],
+      error = false,
+      cancelcb = () => {console.log('cancel cb called for orderbook')},
+      decimals = 10,
+    } = this.props
+
+    if (error) {
+      return <ErrorBox error={error.message} />
     }
 
-    return false
+    return (
+      <div className='orderbook__internal'>
+        <div className='orderbook__title'>
+          <h1 className='orderbook__title-bids'>
+            Bids
+          </h1>
+          <h1 className='orderbook__title-asks'>
+            Asks
+          </h1>
+        </div>
+        <OrderbookSide data={bids} cancelcb={cancelcb} decimals={decimals} side='bids' />
+        <OrderbookSide data={asks} cancelcb={cancelcb} decimals={decimals} side='asks' />
+      </div>
+    )
+  }
+}
+
+class OrderbookSide extends Component {
+  render() {
+    const {
+      data = [],
+      cancelcb,
+      decimals,
+      side,
+    } = this.props
+    return (
+      <div className='orderbook__side'>
+        <div className='orderbook__explaintable rowI'>
+          <div className='row__price'>
+            Price
+          </div>
+          <div className='row__quantity'>
+            Quantity
+          </div>
+          <div className='row__cancelbutton'>
+            Cancel
+          </div>
+        </div>
+        {
+          data.map((dataRow) => {
+            return (<OrderbookRow data={dataRow} cancelcb={cancelcb} decimals={decimals} side={side} />)
+          })
+        }
+      </div>
+    )
+  }
+}
+
+class OrderbookRow extends Component {
+  render() {
+    const {
+      data: {
+        id,
+        account,
+        price,
+        qty,
+        type,
+      },
+      cancelcb,
+      decimals, // TODO is this the same for qty and amount always?
+      side,
+    } = this.props
+
+    return (
+      <div className='rowI'>
+        <div className='row__price'>
+          {price / 10 ** decimals}
+        </div>
+        <div className='row__quantity'>
+          {qty / 10 ** decimals}
+        </div>
+        <div className='row__cancelbutton' onClick={(e) => cancelcb(id, side)}>
+          X
+        </div>
+      </div>
+    )
   }
 }
 
@@ -99,6 +210,103 @@ const Select = (props) => {
       </select>
     </label>
   )
+}
+
+class Positions extends Component {
+  componentDidMount () {
+    const { symbol } = this.props
+
+    this.periodicFetch = setInterval(() => {
+      sb.orders('BTCUSD', {}, (err, res) => {
+        if (err) {
+          return this.setState({ error: err })
+        }
+
+        this.setState({
+          error: false,
+          asks: res.asks,
+          bids: res.bids
+        })
+      })
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.periodicFetch)
+  }
+
+  render () {
+    const {
+      cancelcb,
+      symbol,
+      decimals,
+    } = this.props
+
+    const {
+      bids,
+      asks,
+      error
+    } = this.state
+
+    return (
+      <PositionsInternal
+        bids={bids}
+        asks={asks}
+        error={error}
+        decimals={decimals}
+        cancelcb={(id, side) => {cancelcb(id, symbol, side)}}
+      />
+    )
+  }
+}
+
+class PositionsInternal extends Component {
+  render() {
+    const {
+      bids = [],
+      asks = [],
+      error = false,
+      decimals = 10,
+      cancelcb = () => console.log('cancelcb called for positions'),
+    } = this.props
+
+    if (error) {
+      return <ErrorBox error={error.message} />
+    }
+
+    return (
+      <div className='positions__internal'>
+        <div className='positions__title'>
+          TITLE
+        </div>
+        <div className='orderbook__explaintable rowI'>
+          <div className='row__price'>
+            Price
+          </div>
+          <div className='row__quantity'>
+            Quantity
+          </div>
+          <div className='row__cancelbutton'>
+            Cancel
+          </div>
+        </div>
+        <div className='positions__bids'>
+          {
+            bids.map((bidRow) => {
+              return <OrderbookRow data={bidRow} cancelcb={cancelcb} decimals={decimals} side='bids' />
+            })
+          }
+        </div>
+        <div className='positions__asks'>
+          {
+            asks.map((askRow) => {
+              return <OrderbookRow data={askRow} cancelcb={cancelcb} decimals={decimals} side='asks' />
+            })
+          }
+        </div>
+      </div>
+    )
+  }  
 }
 
 class App extends Component {
