9f816add5f35bf70496d158cf8a2cf439afe1893
Robert Kowalski <rok@kowalski.gd>
2018-07-16 14:54:42 +0200

    extend app

--- test/browser-exchange/jsx/app.jsx
+++ test/browser-exchange/jsx/app.jsx
@@ -19,19 +19,36 @@ const eos = {
   writeNodeConf
 }
 
-let sb = new Sunbeam(eos, { account: 'testuser1234' })
+const sbConf = { account: 'testuser1234' }
+let sb = new Sunbeam(eos, sbConf)
 
 
 class Clock extends Component {
-  render () {
-    let time = new Date()
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      time: new Date()
+    }
+  }
+
+  componentDidMount () {
+    setInterval(() => {
+      this.setState({ time: new Date() })
+    }, 1000)
+  }
+
+  render (props, state) {
+    const { time } = state
     return <time datetime={time.toISOString()}>{ time.toLocaleTimeString() }</time>
   }
 }
 
-const ErrorBox = (text) => {
+const ErrorBox = (props) => {
   return (
-    <div className="">{text}</div>
+    <div className="error">
+      Error: {props.error}
+    </div>
   )
 }
 
@@ -61,17 +78,69 @@ class Orderbook extends Component {
 
   render (props, state) {
     if (state.error) {
-      return <ErrorBox text={state.error} />
+      return <ErrorBox error={state.error.message} />
     }
 
     return false
   }
 }
 
+const Select = (props) => {
+  const { pairs, pair, eventBinding } = props
+
+  const opts = pairs.map((el) => {
+    return <option value={el}>{el}</option>
+  })
+
+  return (
+    <label>
+      <select value={pair} onChange={eventBinding}>
+        {opts}
+      </select>
+    </label>
+  )
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pair: 'BTCUSD'
+    }
+
+    this.pairs = [ 'BTCUSD', 'ETHUSD']
+  }
+
+  onPairChange (event) {
+    this.setState({
+      pair: event.target.value
+    })
+  }
+
+  render (props, state) {
+    return (
+      <div class="container">
+        <div class="row">
+          <div class="column"><Clock /> - {sbConf.account}@active - eosfinex - Pair: {state.pair}</div>
+          <div class="column column-25">
+            <div>
+              <Select
+                pair={this.state.pair}
+                pairs={this.pairs}
+                eventBinding={this.onPairChange.bind(this)}
+              />
+            </div>
+          </div>
+
+        </div>
+        <Orderbook symbol={state.pair} />
+      </div>
+    )
+  }
+}
+
 
 render((
-  <div class="container">
-    <Clock />
-    <Orderbook symbol="BTCUSD" />
-  </div>
+  <App />
 ), document.body)
