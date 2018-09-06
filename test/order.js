45998368de93861194ad2a09f9f11390f0cc1862
Robert Kowalski <rok@kowalski.gd>
2018-09-06 15:31:09 +0200

    orders: add managed orders
    takes snapshots and keeps them up to date.

---
+++ test/order.js
@@ -0,0 +1,109 @@
+/* eslint-env mocha */
+
+'use strict'
+
+const Order = require('../lib/order.js')
+const assert = require('assert')
+
+const conf = {}
+
+describe('order helper', () => {
+  it('sets bid and ask scope', () => {
+    const ask = new Order({
+      symbol: 'BTC.USD',
+      price: '520',
+      amount: '-0.99',
+      type: 'EXCHANGE_LIMIT'
+    }, conf)
+
+    assert.equal(ask.parse().scope, 'btc.usd.a')
+    assert.equal(ask.parse().amount, '9900')
+
+    const bid = new Order({
+      symbol: 'BTC.USD',
+      price: '520',
+      amount: '0.99',
+      type: 'EXCHANGE_LIMIT'
+    }, conf)
+
+    assert.equal(bid.parse().scope, 'btc.usd.b')
+    assert.equal(bid.parse().amount, '9900')
+  })
+
+  it('client id is timestamp by default', () => {
+    const ask = new Order({
+      symbol: 'BTC.USD',
+      amount: '-0.99',
+      type: 'EXCHANGE_MARKET'
+    }, conf)
+
+    const d = Date.now()
+    ask.parse()
+
+    assert.ok(ask.serialize().clId >= d)
+  })
+
+  it('market order sets flags', () => {
+    const ask = new Order({
+      symbol: 'BTC.USD',
+      amount: '-0.99',
+      type: 'EXCHANGE_MARKET'
+    }, conf)
+
+    ask.parse()
+
+    assert.equal(ask.serialize().flags, 4)
+  })
+
+  it('market order flag set respects existing flags', () => {
+    const ask = new Order({
+      symbol: 'BTC.USD',
+      amount: '-0.99',
+      type: 'EXCHANGE_MARKET',
+      flags: 1 // post only
+    }, conf)
+
+    ask.parse()
+
+    assert.equal(ask.serialize().flags, 5)
+  })
+
+  it('market order flag works with order type market order', () => {
+    const ask = new Order({
+      symbol: 'BTC.USD',
+      amount: '-0.99',
+      type: 'EXCHANGE_MARKET',
+      flags: 4
+    }, conf)
+
+    ask.parse()
+
+    assert.equal(ask.serialize().flags, 4)
+  })
+
+  it('post only flag works', () => {
+    const ask = new Order({
+      symbol: 'BTC.USD',
+      amount: '-0.99',
+      type: 'EXCHANGE_LIMIT',
+      flags: 1
+    }, conf)
+
+    ask.parse()
+
+    assert.equal(ask.serialize().flags, 1)
+  })
+
+  it('price must be always present', () => {
+    const ask = new Order({
+      symbol: 'BTC.USD',
+      amount: '-0.99',
+      type: 'EXCHANGE_MARKET',
+      flags: 1
+    }, conf)
+
+    ask.parse()
+
+    assert.equal(ask.serialize().price, '0')
+  })
+})
--- test/orders.js
+++ test/orders.js
@@ -2,122 +2,152 @@
 
 'use strict'
 
-const Order = require('../lib/order.js')
+const Orders = require('../lib/managed-orders.js')
 const assert = require('assert')
 
-const conf = {}
+const snapMsg = [ '0',
+  'os',
+  [ [ '18446744073709551615',
+      null,
+      1536247370903,
+      'BTC.USD',
+      1536247373000987,
+      1536247378501049,
+      0.9,
+      1,
+      'LIMIT',
+      null,
+      null,
+      null,
+      0,
+      'PARTIALLY FILLED',
+      null,
+      null,
+      1,
+      0,
+      0,
+      0,
+      null,
+      null,
+      null,
+      0,
+      0,
+      0,
+      null,
+      null,
+      'API>EFX',
+      null,
+      null,
+      null ],
+    [ '18446744073709551615',
+        null,
+        1536247370903,
+        'ETH.USD',
+        1536247373000987,
+        1536247378501049,
+        0.9,
+        1,
+        'LIMIT',
+        null,
+        null,
+        null,
+        0,
+        'PARTIALLY FILLED',
+        null,
+        null,
+        1,
+        0,
+        0,
+        0,
+        null,
+        null,
+        null,
+        0,
+        0,
+        0,
+        null,
+        null,
+        'API>EFX',
+        null,
+        null,
+        null ] ] ]
 
-describe('order helper', () => {
-  it('sets bid and ask scope', () => {
-    const ask = new Order({
-      symbol: 'BTC.USD',
-      price: '520',
-      amount: '-0.99',
-      type: 'EXCHANGE_LIMIT'
-    }, conf)
+const emptySnapMsg = [ '0', 'os', [] ]
 
-    assert.equal(ask.parse().scope, 'btc.usd.a')
-    assert.equal(ask.parse().amount, '9900')
+const ocMsg = ["0","oc",[
+  "18446744073709551615",null,12345,"BTC.USD",1536325281501278,
+  1536325337500662,0.000000,10.000000,"LIMIT",null,null,null,0,
+  "fully.filled",null,null,500.000000,0,0,0,null,null,null,0,0,0,null,null,
+  "API>EFX",null,null,null ]]
 
-    const bid = new Order({
-      symbol: 'BTC.USD',
-      price: '520',
-      amount: '0.99',
-      type: 'EXCHANGE_LIMIT'
-    }, conf)
 
-    assert.equal(bid.parse().scope, 'btc.usd.b')
-    assert.equal(bid.parse().amount, '9900')
+const onMsg = ["0","on",[
+  "1",null,12345,"BTC.USD",1536325337500752,
+  1536325337500752,-1.000000,-1.000000,"LIMIT",null,null,null,0,
+  "ACTIVE",null,null,500.000000,0,0,0,null,null,null,0,0,0,null,null,
+  "API>EFX",null,null,null ]]
+
+const ouMsg = ["0","ou",[
+  "1",null,1337,"BTC.USD",1536325337500612,1536325337500612,
+  -10.000000,-10.000000,"LIMIT",null,null,null,0,"ACTIVE",null,null,
+  500.000000,0,0,0,null,null,null,0,0,0,null,null,
+  "API>EFX",null,null,null]]
+
+describe('orders helper', () => {
+  it('takes snapshots', () => {
+    const o = new Orders()
+
+    const snap = snapMsg[2]
+    o.update(snap)
+
+    assert.equal('18446744073709551615', o.getState()[0][0])
+    assert.deepEqual(o.getState(), snap)
   })
 
-  it('client id is timestamp by default', () => {
-    const ask = new Order({
-      symbol: 'BTC.USD',
-      amount: '-0.99',
-      type: 'EXCHANGE_MARKET'
-    }, conf)
+  it('takes empty snapshots', () => {
+    const o = new Orders()
 
-    const d = Date.now()
-    ask.parse()
+    const snap = emptySnapMsg[2]
+    o.update(snap)
 
-    assert.ok(ask.serialize().clId >= d)
+    assert.deepEqual(o.getState(), [])
   })
 
-  it('market order sets flags', () => {
-    const ask = new Order({
-      symbol: 'BTC.USD',
-      amount: '-0.99',
-      type: 'EXCHANGE_MARKET'
-    }, conf)
+  it('update respect pair as ids can be same across pairs', () => {
+    const o = new Orders()
+    const snap = snapMsg[2]
+    o.update(snap)
 
-    ask.parse()
+    const oc = ocMsg[2]
+    o.update(oc)
 
-    assert.equal(ask.serialize().flags, 4)
+    assert.deepEqual(o.getState(), [ oc, snap[1] ])
   })
 
-  it('market order flag set respects existing flags', () => {
-    const ask = new Order({
-      symbol: 'BTC.USD',
-      amount: '-0.99',
-      type: 'EXCHANGE_MARKET',
-      flags: 1 // post only
-    }, conf)
+  it('update on - new order', () => {
+    const o = new Orders()
+    const snap = snapMsg[2]
+    o.update(snap)
 
-    ask.parse()
+    const on = onMsg[2]
+    o.update(on)
 
-    assert.equal(ask.serialize().flags, 5)
+    assert.deepEqual(o.getState(), [ snap[0], snap[1], on ])
   })
 
-  it('market order flag works with order type market order', () => {
-    const ask = new Order({
-      symbol: 'BTC.USD',
-      amount: '-0.99',
-      type: 'EXCHANGE_MARKET',
-      flags: 4
-    }, conf)
+  it('update ou', () => {
+    const o = new Orders()
+    const snap = snapMsg[2]
+    o.update(snap)
 
-    ask.parse()
+    // add order
+    const on = onMsg[2]
+    o.update(on)
+    assert.deepEqual(o.getState(), [ snap[0], snap[1], on ])
 
-    assert.equal(ask.serialize().flags, 4)
-  })
-
-  it('post only flag works', () => {
-    const ask = new Order({
-      symbol: 'BTC.USD',
-      amount: '-0.99',
-      type: 'EXCHANGE_LIMIT',
-      flags: 1
-    }, conf)
-
-    ask.parse()
-
-    assert.equal(ask.serialize().flags, 1)
-  })
-
-  it('price must be always present', () => {
-    const ask = new Order({
-      symbol: 'BTC.USD',
-      amount: '-0.99',
-      type: 'EXCHANGE_MARKET',
-      flags: 1
-    }, conf)
-
-    ask.parse()
-
-    assert.equal(ask.serialize().price, '0')
-  })
-
-  it('prices and amounts float32 issue', () => {
-    const ask = new Order({
-      symbol: 'BTC.USD',
-      amount: '0.678',
-      price: '0.678',
-      type: 'EXCHANGE_LIMIT'
-    }, conf)
-
-    ask.parse()
-
-    assert.equal(ask.serialize().price, '6780')
-    assert.equal(ask.serialize().qty, '6780')
+    // update it
+    const ou = ouMsg[2]
+    o.update(ou)
+    assert.deepEqual(o.getState(), [ snap[0], snap[1], ou ])
   })
 })
