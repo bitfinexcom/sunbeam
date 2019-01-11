7cff375ae90ba89b5434746f636b2bba64df5b09
Robert Kowalski <rok@kowalski.gd>
2019-01-11 11:44:44 +0100

    orders: add oc message support
     - uses mandelbrot base class

--- test/orders.js
+++ test/orders.js
@@ -74,14 +74,14 @@ const snapMsg = [ '0',
 
 const emptySnapMsg = [ '0', 'os', [] ]
 
-const ocMsg = ['0', 'oc', [
-  '18446744073709551615', null, 12345, 'BTC.USD', 1536325281501278,
-  1536325337500662, 0.000000, 10.000000, 'LIMIT', null, null, null, 0,
-  'fully.filled', null, null, 500.000000, 0, 0, 0, null, null, null, 0, 0, 0, null, null,
-  'API>EFX', null, null, null ]]
-
 const onMsg = ['0', 'on', [
   '1', null, 1234578910, 'BTC.USD', 1536325337500752,
+  1536325337500752, -1.000000, -1.000000, 'LIMIT', null, null, null, 0,
+  'ACTIVE', null, null, 500.000000, 0, 0, 0, null, null, null, 0, 0, 0, null, null,
+  'API>EFX', null, null, null ]]
+
+const onMsgEOS = ['0', 'on', [
+  '1', null, 1234578910, 'EOS.USD', 1536325337500752,
   1536325337500752, -1.000000, -1.000000, 'LIMIT', null, null, null, 0,
   'ACTIVE', null, null, 500.000000, 0, 0, 0, null, null, null, 0, 0, 0, null, null,
   'API>EFX', null, null, null ]]
@@ -91,6 +91,11 @@ const ouMsg = ['0', 'ou', [
   -10.000000, -10.000000, 'LIMIT', null, null, null, 0, 'ACTIVE', null, null,
   500.000000, 0, 0, 0, null, null, null, 0, 0, 0, null, null,
   'API>EFX', null, null, null]]
+
+const ocMsg = [ '0', 'oc', [
+  '1', null, 1234578910, 'BTC.USD', 1547142966504439,
+  1547143021501823, 0, 10.99, 'LIMIT', null, null, null, 0, 'fully.filled', null, null,
+  3, 0, 0, 0, null, null, null, 0, 0, 0, null, null, 'API>EFX', null, null, null ], 1 ]
 
 describe('orders helper', () => {
   it('takes snapshots', () => {
@@ -111,13 +116,13 @@ describe('orders helper', () => {
     assert.deepStrictEqual(o.getState(), [])
   })
 
-  it('update respect pair as ids can be same across pairs', () => {
+  it('update respects pair as ids can be same across pairs', () => {
     const o = new Orders()
     o.update(snapMsg)
-    o.update(ocMsg)
+    o.update(onMsgEOS)
 
     const snap = snapMsg[2]
-    assert.deepStrictEqual(o.getState(), [ ocMsg[2], snap[1] ])
+    assert.deepStrictEqual(o.getState(), [ snap[0], snap[1], onMsgEOS[2] ])
   })
 
   it('update on - new order', () => {
@@ -141,6 +146,21 @@ describe('orders helper', () => {
     // update it
     o.update(ouMsg)
     assert.deepStrictEqual(o.getState(), [ snap[0], snap[1], ouMsg[2] ])
+  })
+
+  it('update oc', () => {
+    const o = new Orders()
+    o.update(snapMsg)
+
+    const snap = snapMsg[2]
+    // add order
+    o.update(onMsg)
+    assert.deepStrictEqual(o.getState(), [ snap[0], snap[1], onMsg[2] ])
+
+    // delete it
+    o.update(ocMsg)
+
+    assert.deepStrictEqual(o.getState(), [ snap[0], snap[1] ])
   })
 
   it('supports keyed format, snaps', () => {
@@ -218,5 +238,21 @@ describe('orders helper', () => {
 
     assert.deepStrictEqual(o.getState()[2], expUpdt)
     assert.strictEqual(o.getState().length, 3)
+  })
+
+  it('supports keyed format, delete', () => {
+    const o = new Orders({ keyed: true })
+    o.update(snapMsg)
+    o.update(onMsg)
+
+    assert.strictEqual(o.getState().length, 3)
+    o.update(ocMsg)
+
+    const state = o.getState()
+    assert.deepStrictEqual(
+      ['18446744073709551615', '18446744073709551615'],
+      [state[0].id, state[1].id]
+    )
+    assert.strictEqual(o.getState().length, 2)
   })
 })
